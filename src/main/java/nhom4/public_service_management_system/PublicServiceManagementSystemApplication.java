5bbf9dfb47d8bdbaec685620ccb5ba8f02ad092f
pnt2005 <phannthanh2005@gmail.com>
2026-08-16 23:17:44 +0700

    init project

---
+++ src/main/java/nhom4/public_service_management_system/PublicServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package nhom4.public_service_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicServiceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nhom4/public_service_management_system/PublicServiceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nhom4.public_service_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicServiceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ce4e2647896789525a6264671ccb2e8b47e4cb44
pnt2005 <phannthanh2005@gmail.com>
2026-08-17 10:31:30 +0700

    feat: implement global exception handling with custom error responses and add Lombok dependency

---
+++ src/main/java/nhom4/public_service_management_system/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package nhom4.public_service_management_system.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
